95300740ea411ae3d05be3c0066f9fbf67cba55c
richadchin <66206074+richadchin@users.noreply.github.com>
2022-05-19 17:33:41 +0800

    error in setting
    must check why in django join dir

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import List
+#from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 def home(request):
-    return render(request, 'home.html', {})
+    all_items = List.objects.all
+    return render(request, 'home.html', {'all_items':all_items })
+def about(request):
+    return render(request, 'about.html', {})
